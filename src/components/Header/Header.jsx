abc79b8e19c8bd4ec02da7fdb3643f5a3150b3e9
JosefKorba <josef.korba1@gmail.com>
2024-03-15 10:42:12 +0100

    imported components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
-import styles from './Header.module.css';
+import styles from './Header.module.css'
+import Logo from '../Logo/Logo'
+import NavBar from '../NavBar/NavBar';
+import Cart from '../Cart/Cart';
 
 const Header = () => {
  return (
     <header className={styles.header}>
-      
+      <Logo />
+      <NavBar />
+      <Cart />
     </header>
  );
 };
