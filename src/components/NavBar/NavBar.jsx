dbc76971eb96909fa7f8fc0a26b9ead37b3891d7
JosefKorba <josef.korba1@gmail.com>
2024-03-19 00:38:27 +0100

    refactored className link

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,10 +3,10 @@ import styles from "./NavBar.module.css";
 const NavBar = () => {
   return <nav>
         <ul className={styles.siteNavigation}>
-          <li className={styles.link}><a href="#">HOME</a></li>
-          <li className={styles.link}><a href="#">HEADPHONES</a></li>
-          <li className={styles.link}><a href="#">SPEAKERS</a></li>
-          <li className={styles.link}><a href="#">EARPHONES</a></li>
+          <li><a className={styles.link} href="#">HOME</a></li>
+          <li><a className={styles.link} href="#">HEADPHONES</a></li>
+          <li><a className={styles.link} href="#">SPEAKERS</a></li>
+          <li><a className={styles.link} href="#">EARPHONES</a></li>
         </ul>      
   </nav>;
 };
