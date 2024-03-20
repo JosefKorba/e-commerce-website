4babb85367104e2f9c5f5bd901a90e6e329c7b94
JosefKorba <josef.korba1@gmail.com>
2024-03-20 14:44:14 +0100

    added product fetching in Hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,27 @@
+import { useState, useEffect } from "react";
 import styles from "./Hero.module.css";
+import data from "../../data.json";
+
 
 const Hero = () => {
+  const [product, setProduct] = useState("");
+
+  useEffect(() => {
+    // Retrieving a product from data.json
+    const product = data.find(item => item.id === 4);
+    setProduct(product);
+  }, []);
+
   return (
-    <div className={styles.heroContainer}>
+    <section className={styles.heroContainer}>
+      <article className={styles.heroText} >
+        <p>New Product</p>
+        <h1>{product.name}</h1>
+        <p>{product.heroText}</p>
+        <button>SEE PRODUCT</button>
+      </article>
+    </section>
+  );
+};
 
-    </div>
-  )
-}
-
-export default Hero
+export default Hero;
