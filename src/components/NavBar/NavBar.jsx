d8671ea878e7ccff0be0b0055b8fda9a14973c1c
JosefKorba <josef.korba1@gmail.com>
2024-03-22 12:29:31 +0100

    updated CSS NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,16 +3,20 @@ import Logo from "../Logo/Logo";
 import Cart from "../Cart/Cart";
 
 const NavBar = () => {
-  return <nav className={styles.navBarCotainer}>
-    <Logo />
+  return (
+    <div className={styles.navBarBackground}> 
+      <nav className={styles.navBarContainer}> 
+        <Logo />
         <ul className={styles.siteNavigation}>
           <li><a className={styles.link} href="#">HOME</a></li>
           <li><a className={styles.link} href="#">HEADPHONES</a></li>
           <li><a className={styles.link} href="#">SPEAKERS</a></li>
           <li><a className={styles.link} href="#">EARPHONES</a></li>
         </ul> 
-    <Cart />     
-  </nav>;
+        <Cart />
+      </nav>
+    </div>
+  );
 };
 
 export default NavBar;
