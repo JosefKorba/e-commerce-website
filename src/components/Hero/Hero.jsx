d94488429efc82d75a8d78aa516c0e6f05b60ed5
JosefKorba <josef.korba1@gmail.com>
2024-03-22 12:30:55 +0100

    updated CSS Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,22 +3,23 @@ import styles from "./Hero.module.css";
 import data from "../../data.json";
 import Button from "../Button/Button";
 
-
 const Hero = () => {
   const [product, setProduct] = useState("");
 
   useEffect(() => {
     // Retrieving a product from data.json
-    const product = data.find(item => item.id === 4);
+    const product = data.find((item) => item.id === 4);
     setProduct(product);
   }, []);
 
   return (
     <section className={styles.heroContainer}>
+      <div className={styles.heroContent}>
         <p>NEW PRODUCT</p>
         <h1 className={styles.heroTitle}>{product.name}</h1>
         <p className={styles.heroText}>{product.heroText}</p>
         <Button color="darkOrange">SEE PRODUCT</Button>
+      </div>
     </section>
   );
 };
