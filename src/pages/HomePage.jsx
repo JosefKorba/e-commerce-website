bc809dad6c8b0a2f8882c2ee42a637520ad88152
JosefKorba <josef.korba1@gmail.com>
2024-03-25 17:56:51 +0100

    created HomePage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import NavBar from "../components/NavBar/NavBar";
+import Hero from "../components/Hero/Hero";
+
+const HomePage = () => {
+  return (
+    <div>
+      <NavBar />
+      <Hero />
+    </div>
+  );
+};
+
+export default HomePage;
