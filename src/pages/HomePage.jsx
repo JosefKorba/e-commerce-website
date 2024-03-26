3dde636b4823e2291492986a2ffa8e40032a8978
JosefKorba <josef.korba1@gmail.com>
2024-03-26 20:27:10 +0100

    imported CategoryCard to HomPage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import NavBar from "../components/NavBar/NavBar";
 import Hero from "../components/Hero/Hero";
+import CategoryCard from "../components/CategoryCard/CategoryCard";
 
 const HomePage = () => {
   return (
     <div>
       <NavBar />
       <Hero />
+      <CategoryCard />
     </div>
   );
 };
