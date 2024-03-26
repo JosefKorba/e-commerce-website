94e91143547685cf0d789b1169fe3f2076381381
JosefKorba <josef.korba1@gmail.com>
2024-03-26 20:23:59 +0100

    added CategoryCard component

--- src/components/CategoryCard/CategoryCard.jsx
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -1,11 +1,32 @@
-import './CategoryCard.css';
+
+import { Link } from 'react-router-dom';
+import styles from "./CategoryCard.module.css";
+
+
+import headphonesImage from '../../assets/images/shared/desktop/image-category-thumbnail-headphones.png';
+import speakersImage from '../../assets/images/shared/desktop/image-category-thumbnail-speakers.png';
+import earphonesImage from '../../assets/images/shared/desktop/image-category-thumbnail-earphones.png';
+
+const categories = [
+ { name: 'HEADPHONES', image: headphonesImage, link: '/headphones' },
+ { name: 'SPEAKERS', image: speakersImage, link: '/speakers' },
+ { name: 'EARPHONES', image: earphonesImage, link: '/earphones' }
+];
 
 const CategoryCard = () => {
   return (
-    <section>
-        
+    <section className={styles.categorySection}>
+      {categories.map(category => (
+        <Link to={category.link} key={category.name}>
+          <article className={styles.categoryCard}>
+            <img src={category.image} alt={category.name} className={styles.categoryImage} />
+            <h6 className={styles.categoryName}>{category.name}</h6>
+            <p className={styles.shopText}>SHOP&gt;</p>
+          </article>
+        </Link>
+      ))}
     </section>
-  )
-}
+  );
+};
 
-export default CategoryCard
+export default CategoryCard;
