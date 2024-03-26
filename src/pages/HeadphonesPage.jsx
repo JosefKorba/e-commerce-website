c4631eea64d057d66b83d74b8cb90776fd3795d0
JosefKorba <josef.korba1@gmail.com>
2024-03-26 20:26:48 +0100

    added HeadphonesPage

---
+++ src/pages/HeadphonesPage.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../components/NavBar/NavBar";
+
+
+const HeadphonesPage = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+};
+
+export default HeadphonesPage;
