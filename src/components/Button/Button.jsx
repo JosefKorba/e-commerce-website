1f927f32d9d64c8dab89006120c68000cb43d192
JosefKorba <josef.korba1@gmail.com>
2024-03-27 18:46:14 +0100

    updated proptypes in Button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,12 +1,19 @@
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import styles from './Button.module.css'; 
 
 const Button = ({ path, children, color }) => {
- return (
+  return (
     <Link to={path}>
       <button className={`${styles.button} ${styles[color]}`}>{children}</button>
     </Link>
- );
+  );
+};
+
+Button.propTypes = {
+  path: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+  color: PropTypes.string,
 };
 
 export default Button;
