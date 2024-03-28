ff64337b78940ab65e4c67aceb7e766a39510fda
JosefKorba <josef.korba1@gmail.com>
2024-03-28 19:22:44 +0100

    imported pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-
+import HeadphonesPage from "./pages/HeadphonesPage"; 
+import SpeakersPage from "./pages/SpeakersPage"; 
+import EarphonesPage from "./pages/EarphonesPage"; 
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/headphones" element={<HeadphonesPage />} /> 
+        <Route path="/speakers" element={<SpeakersPage />} /> 
+        <Route path="/earphones" element={<EarphonesPage />} /> 
       </Routes>
     </Router>
   );
