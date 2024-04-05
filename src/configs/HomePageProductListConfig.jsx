b287804484166fa9b33aa02505d5f60110f9b05e
JosefKorba <josef.korba1@gmail.com>
2024-04-05 14:14:08 +0200

    created HomePageProductList Config

---
+++ src/configs/HomePageProductListConfig.jsx
@@ -0,0 +1,18 @@
+export default {
+   "product1": {
+      title: "ZX9 Speaker",
+      description: "Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.",
+      image: "src/assets/images/home/desktop/image-speaker-zx9.png",
+      type: "large" 
+   },
+   "product2": {
+      title: "ZX7 Speaker",
+      image: "src/assets/images/home/desktop/image-speaker-zx7.jpg",
+      type: "medium" 
+   },
+   "product3": {
+      title: "YX1 Wireless Earphones",
+      image: "src/assets/images/home/desktop/image-earphones-yx1.jpg",
+      type: "small" 
+   },
+  };
