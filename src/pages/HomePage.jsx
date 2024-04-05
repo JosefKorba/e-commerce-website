c1424429856b60dd2f66c5d6a606f30fb980b699
JosefKorba <josef.korba1@gmail.com>
2024-04-05 14:14:59 +0200

    imported HomePageProductList to HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,17 @@
 import NavBar from "../components/Global/NavBar/NavBar";
 import Hero from "../components/Hero/Hero";
 import CategoryCard from "../components/Global/CategoryCard/CategoryCard";
+import HomePageProductList from "../components/HomePageProductList/HomePageProductList";
 
 const HomePage = () => {
-  return (
+ return (
     <div>
       <NavBar />
       <Hero />
       <CategoryCard />
+      <HomePageProductList />
     </div>
-  );
+ );
 };
 
 export default HomePage;
