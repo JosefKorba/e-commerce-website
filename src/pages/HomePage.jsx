26908a53f10c0cf2984091ab918fe61e7ec5eb48
JosefKorba <josef.korba1@gmail.com>
2024-04-11 10:27:31 +0200

    imported components

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,24 @@
-import NavBar from "../components/Global/NavBar/NavBar";
+import Header from "../components/Global/Header/Header";
 import Hero from "../components/Hero/Hero";
 import CategoryCard from "../components/Global/CategoryCard/CategoryCard";
 import HomePageProductList from "../components/HomePageProductList/HomePageProductList";
+import BestAudioGearSection from "../components/Global/BestAudioGearSection/BestAudioGearSection";
+import Footer from "../components/Global/Footer/Footer";
 
 const HomePage = () => {
  return (
-    <div>
-      <NavBar />
+    <>
+      <Header />
       <Hero />
       <CategoryCard />
       <HomePageProductList />
-    </div>
+      <BestAudioGearSection
+      imageSrc="src\assets\images\shared\desktop\image-best-gear.jpg"
+      title="Bringing you the best audio gear"
+      description="We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment."
+      />
+      <Footer />
+    </>
  );
 };
 
--- src/pages/SpeakersPage.jsx
+++ src/pages/SpeakersPage.jsx
@@ -1,11 +1,19 @@
-import NavBar from "../components/Global/NavBar/NavBar";
+import Header from "../components/Global/Header/Header";
+import BestAudioGearSection from "../components/Global/BestAudioGearSection/BestAudioGearSection";
+import CategoryCard from "../components/Global/CategoryCard/CategoryCard";
 
 
 const SpeakersPage = () => {
   return (
-    <div>
-      <NavBar />
-    </div>
+    <>
+      <Header />
+      <CategoryCard />
+      <BestAudioGearSection
+      imageSrc="src\assets\images\shared\desktop\image-best-gear.jpg"
+      title="Bringing you the best audio gear"
+      description="We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment."
+    />
+    </>
   );
 };
 
