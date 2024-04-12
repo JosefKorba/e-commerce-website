9bc84c7870050ad8d5d2bf72e1687a78ac7a3d34
JosefKorba <josef.korba1@gmail.com>
2024-04-12 09:42:15 +0200

    imported Footer component

--- src/pages/HeadphonesPage.jsx
+++ src/pages/HeadphonesPage.jsx
@@ -1,6 +1,7 @@
 import Header from "../components/Global/Header/Header";
 import BestAudioGearSection from "../components/Global/BestAudioGearSection/BestAudioGearSection";
 import CategoryCard from "../components/Global/CategoryCard/CategoryCard";
+import Footer from "../components/Global/Footer/Footer";
 
 const HeadphonesPage = () => {
   return (
@@ -11,7 +12,8 @@ const HeadphonesPage = () => {
       imageSrc="src\assets\images\shared\desktop\image-best-gear.jpg"
       title="Bringing you the best audio gear"
       description="We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment."
-    />
+      />
+      <Footer />
     </>
   );
 };
--- src/pages/SpeakersPage.jsx
+++ src/pages/SpeakersPage.jsx
@@ -1,6 +1,7 @@
 import Header from "../components/Global/Header/Header";
 import BestAudioGearSection from "../components/Global/BestAudioGearSection/BestAudioGearSection";
 import CategoryCard from "../components/Global/CategoryCard/CategoryCard";
+import Footer from "../components/Global/Footer/Footer";
 
 
 const SpeakersPage = () => {
@@ -12,7 +13,8 @@ const SpeakersPage = () => {
       imageSrc="src\assets\images\shared\desktop\image-best-gear.jpg"
       title="Bringing you the best audio gear"
       description="We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment."
-    />
+     />
+     <Footer />
     </>
   );
 };
