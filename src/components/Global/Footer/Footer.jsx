449dd06e4b0b535c84d3f6c80720a66bc3510f11
JosefKorba <josef.korba1@gmail.com>
2024-04-12 09:39:54 +0200

    update section in Footer

--- src/components/Global/Footer/Footer.jsx
+++ src/components/Global/Footer/Footer.jsx
@@ -1,16 +1,58 @@
-import Logo from "../Logo/Logo"
-import NavBar from "../NavBar/NavBar"
-import styles from "./Footer.module.css"
+import Logo from "../Logo/Logo";
+import NavBar from "../NavBar/NavBar";
+import styles from "./Footer.module.css";
+import facebookIcon from "../../../assets/images/shared/desktop/icon-facebook.svg";
+import twitterIcon from "../../../assets/images/shared/desktop/icon-twitter.svg";
+import instagramIcon from "../../../assets/images/shared/desktop/icon-instagram.svg";
 
 const Footer = () => {
   return (
-    <div className={styles.footerBackground}>
-        <nav className={styles.footerContainer}>
-            <Logo />
-            <NavBar />
+    <footer className={styles.footerBackground}>
+      <nav className={styles.footerNavContainer} aria-label="Main navigation">
+        <Logo />
+        <NavBar />
+      </nav>
+      <section className={styles.footerDescriptionContainer}>
+        <div className={styles.footerDescription}>
+          <p className={styles.footerDescriptionText}>
+            Audiophile is an all in one stop to fulfill your audio needs. We're
+            a small team of music lovers and sound specialists who are devoted
+            to helping you get the most out of personal audio. Come and visit
+            our demo facility - we’re open 7 days a week.
+          </p>
+          <p className={styles.footerDescriptionCopyright}>
+            Copyright 2024. All Rights Reserved
+          </p>
+        </div>
+        <nav className={styles.footerSocialMediaNav} aria-label="Social media">
+          <a
+            className={styles.footerSocialMediaLink}
+            href="https://www.facebook.com/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <img src={facebookIcon} alt="Facebook" />
+          </a>
+          <a
+            className={styles.footerSocialMediaLink}
+            href="https://www.twitter.com/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <img src={twitterIcon} alt="Twitter" />
+          </a>
+          <a
+            className={styles.footerSocialMediaLink}
+            href="https://www.instagram.com/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <img src={instagramIcon} alt="Instagram" />
+          </a>
         </nav>
-    </div>
-  )
-}
+      </section>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
