dd660eb84626e37da7d7637d732305008cff6e56
JosefKorba <josef.korba1@gmail.com>
2024-04-18 17:19:10 +0200

    added assets

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  assetsInclude: ['src/assets/images/**/*.jpg', 'src/assets/images/**/*.jpeg', 'src/assets/images/**/*.png', 'src/assets/images/**/*.gif', 'src/assets/images/**/*.svg'],
+});
