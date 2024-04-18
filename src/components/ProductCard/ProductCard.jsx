90e2ac4a7fe40b1ec4d5da6401a4d5e95584cbfa
JosefKorba <josef.korba1@gmail.com>
2024-04-18 17:18:10 +0200

    created ProductCard component

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Button from '../Global/Button/Button';
+import styles from './ProductCard.module.css'; 
+
+const ProductCard = ({ product, productPath }) => {
+ const [screenSize, setScreenSize] = useState('desktop'); 
+
+ useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 600) {
+        setScreenSize('mobile');
+      } else if (window.innerWidth <= 900) {
+        setScreenSize('tablet');
+      } else {
+        setScreenSize('desktop');
+      }
+    };
+
+   
+    handleResize();
+
+    
+    window.addEventListener('resize', handleResize);
+
+  
+    return () => window.removeEventListener('resize', handleResize);
+ }, []); 
+
+ console.log("product.categoryImage:", product.categoryImage);
+ console.log("screenSize:", screenSize);
+
+ const imageSrc = product.categoryImage[screenSize];
+
+ console.log("imageSrc:", imageSrc);
+ return (
+    <div className={styles.productCard}>
+      <img src={imageSrc} alt={product.name} className={styles.productImage} />
+      <div className={styles.productInfo}>
+        <h2 className={styles.productTitle}>{product.name}</h2>
+        <p className={styles.productDescription}>{product.description}</p>
+        <Button path={productPath} color="darkOrange" ariaLabel="View Product Details">SEE PRODUCT</Button>
+      </div>
+    </div>
+ );
+};
+
+
+ProductCard.propTypes = {
+ product: PropTypes.shape({
+    categoryImage: PropTypes.object.isRequired,
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+ }).isRequired,
+ productPath: PropTypes.string.isRequired,
+};
+
+export default ProductCard;
+
