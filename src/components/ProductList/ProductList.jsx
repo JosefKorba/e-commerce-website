745971a5bebc2340ec3fbf232d9a6a672105d419
JosefKorba <josef.korba1@gmail.com>
2024-04-22 17:45:22 +0200

    enhanced ProductList with dynamic index and updated PropTypes

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,25 +1,23 @@
+
 import ProductCard from '../ProductCard/ProductCard';
-import styles from './ProductList.module.css'; 
 import PropTypes from 'prop-types';
 
 const ProductList = ({ products }) => {
- return (
-     <div className={styles.productList}>
-       {products.map((product) => (
-         <ProductCard key={product.id} product={product} screenSize="desktop" productPath={`/product/${product.slug}`} /> 
-       ))}
-     </div>
- );
+  return (
+    <div>
+      {products.map((product, index) => (
+        <ProductCard key={product.id} product={product} index={index} productPath={`/product/${product.slug}`} />
+      ))}
+    </div>
+  );
 };
 
 ProductList.propTypes = {
-    products: PropTypes.arrayOf(
-       PropTypes.shape({
-         image: PropTypes.object.isRequired,
-         name: PropTypes.string.isRequired,
-         description: PropTypes.string.isRequired,
-       })
-    ).isRequired,
-   };
+  products: PropTypes.arrayOf(PropTypes.shape({
+    categoryImage: PropTypes.object.isRequired,
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  })).isRequired,
+};
 
 export default ProductList;
