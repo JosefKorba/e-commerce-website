d7019964af1af55b99167118c1992fe2dd48db89
JosefKorba <josef.korba1@gmail.com>
2024-05-03 14:51:55 +0200

    added CategoryHeader component for displaying category titles

---
+++ src/components/Global/CategoryHeader/CategoryHeader.jsx
@@ -0,0 +1,16 @@
+import styles from "./CategoryHeader.module.css";
+import PropTypes from "prop-types";
+
+const CategoryHeader = ({ categoryName }) => {
+  return (
+    <header className={styles.categoryBackground}>
+      <h2 className={styles.categoryTitle}>{categoryName}</h2>
+    </header>
+  );
+};
+
+CategoryHeader.propTypes = {
+  categoryName: PropTypes.string.isRequired,
+};
+
+export default CategoryHeader;
