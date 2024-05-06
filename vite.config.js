5c9d568bd08cee84cdf5ee2c3626427629a3adaa
JosefKorba <josef.korba1@gmail.com>
2024-05-06 12:24:06 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['src/assets/images/**/*.jpg', 'src/assets/images/**/*.jpeg', 'src/assets/images/**/*.png', 'src/assets/images/**/*.gif', 'src/assets/images/**/*.svg'],
+  assetsInclude: [
+    "src/assets/images/**/*.jpg",
+    "src/assets/images/**/*.jpeg",
+    "src/assets/images/**/*.png",
+    "src/assets/images/**/*.gif",
+    "src/assets/images/**/*.svg",
+  ],
+  build: {
+    outDir: "dist",
+  },
 });
