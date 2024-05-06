a9bb43f3dc6de60635562bb528f7b77dbb469eac
JosefKorba <josef.korba1@gmail.com>
2024-05-06 12:12:25 +0200

    import ProductDetailPage and CheckoutPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import HeadphonesPage from "./pages/HeadphonesPage"; 
-import SpeakersPage from "./pages/SpeakersPage"; 
-import EarphonesPage from "./pages/EarphonesPage"; 
+import HeadphonesPage from "./pages/HeadphonesPage";
+import SpeakersPage from "./pages/SpeakersPage";
+import EarphonesPage from "./pages/EarphonesPage";
+import ProductDetailPage from "./pages/ProductDetailPage";
+import CheckoutPage from './pages/CheckoutPage';
 
 function App() {
-  return (
+ return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/headphones" element={<HeadphonesPage />} /> 
-        <Route path="/speakers" element={<SpeakersPage />} /> 
-        <Route path="/earphones" element={<EarphonesPage />} /> 
+        <Route path="/headphones" element={<HeadphonesPage />} />
+        <Route path="/speakers" element={<SpeakersPage />} />
+        <Route path="/earphones" element={<EarphonesPage />} />
+        <Route path="/:category/:productId" element={<ProductDetailPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
       </Routes>
     </Router>
-  );
+ );
 }
 
 export default App;
