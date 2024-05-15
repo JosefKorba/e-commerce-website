e69a2073136289fcad7b144b43eee76a9e866a2c
JosefKorba <josef.korba1@gmail.com>
2024-05-16 01:09:11 +0200

    update Content component to display items in the box

--- src/components/ProductDetailPage/Content/Content.jsx
+++ src/components/ProductDetailPage/Content/Content.jsx
@@ -1,8 +1,33 @@
+import PropTypes from 'prop-types';
+import styles from './Content.module.css';
 
-const Content = () => {
+const Content = ({ items }) => {
+  if (!items) {
+    return <div className={styles.content}>No items available</div>;
+  }
+
   return (
-    <div>Content</div>
-  )
-}
+    <div className={styles.content}>
+      <h3 className={styles.contentTitle}>In the Box</h3>
+      <ul className={styles.contentList}>
+        {items.map((item, index) => (
+          <li key={index} className={styles.contentItem}>
+            <span className={styles.itemQuantity}>{item.quantity}x</span>
+            <span className={styles.itemName}>{item.item}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default Content
+Content.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      quantity: PropTypes.number.isRequired,
+      item: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+export default Content;
