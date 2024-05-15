fec933b8ed1ab0a9ec4a04a5138a3df47332747b
JosefKorba <josef.korba1@gmail.com>
2024-05-16 01:02:13 +0200

    add ProductList component to render a list of ProductCard components

---
+++ src/components/ProductCategoryPages/ProductList/ProductList.jsx
@@ -0,0 +1,29 @@
+import ProductCard from "../ProductCard/ProductCard";
+import PropTypes from "prop-types";
+
+const ProductList = ({ products }) => {
+  return (
+    <div>
+      {products.map((product, index) => (
+        <ProductCard
+          key={product.id}
+          product={product}
+          index={index}
+          productPath={`/product/${product.slug}`}
+        />
+      ))}
+    </div>
+  );
+};
+
+ProductList.propTypes = {
+  products: PropTypes.arrayOf(
+    PropTypes.shape({
+      categoryImage: PropTypes.object.isRequired,
+      name: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+export default ProductList;
