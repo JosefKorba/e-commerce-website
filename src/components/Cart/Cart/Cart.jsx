799cf063b8791dedec970295bf9f9d23dc65e06b
JosefKorba <josef.korba1@gmail.com>
2024-05-22 16:28:40 +0200

    format code with Prettier

--- src/components/Cart/Cart/Cart.jsx
+++ src/components/Cart/Cart/Cart.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
-import useCart from '../../../hooks/useCart';
+import { useState } from "react";
+import useCart from "../../../hooks/useCart";
 import styles from "./Cart.module.css";
 import cartImage from "../../../assets/images/shared/desktop/icon-cart.svg";
-import CartDropdown from '../CartDropdown/CartDropdown';
+import CartDropdown from "../CartDropdown/CartDropdown";
 
 const Cart = () => {
   const { cartItems } = useCart();
@@ -10,10 +10,7 @@ const Cart = () => {
 
   const toggleCartDropdown = () => setIsVisible(!isVisible);
 
-  const handleCheckout = () => {
-    
-   
-  };
+  const handleCheckout = () => {};
 
   return (
     <div className={styles.cartIcon}>
@@ -25,7 +22,9 @@ const Cart = () => {
           </span>
         )}
       </button>
-      {isVisible && <CartDropdown cartItems={cartItems} onCheckout={handleCheckout} />}
+      {isVisible && (
+        <CartDropdown cartItems={cartItems} onCheckout={handleCheckout} />
+      )}
     </div>
   );
 };
