5c83d0e7d6e7bc8d53fdb7351cfc72c3fb08d2b4
JosefKorba <josef.korba1@gmail.com>
2024-05-23 11:22:47 +0200

    update paths for Vite and Netlify compatibility

--- src/configs/HomePageProductListConfig.jsx
+++ src/configs/HomePageProductListConfig.jsx
@@ -2,7 +2,7 @@ export default {
    "product1": {
       title: "ZX9 Speaker",
       description: "Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.",
-      image: "src/assets/images/home/desktop/image-speaker-zx9.png",
+      image: "/images/home/desktop/image-speaker-zx9.png",
       type: "large",
       id: 1, 
       category: "speakers", 
@@ -11,7 +11,7 @@ export default {
    "product2": {
       title: "ZX7 Speaker",
       description: "Experience the next level of sound with our ZX7 Speaker.",
-      image: "src/assets/images/home/desktop/image-speaker-zx7.jpg",
+      image: "/images/home/desktop/image-speaker-zx7.jpg",
       type: "medium",
       id: 2,
       category: "speakers",
@@ -20,7 +20,7 @@ export default {
    "product3": {
       title: "YX1 Earphones",
       description: "Enjoy the freedom of wireless music with our YX1 Earphones.",
-      image: "src/assets/images/home/desktop/image-earphones-yx1.jpg",
+      image: "/images/home/desktop/image-earphones-yx1.jpg",
       type: "small",
       id: 3,
       category: "earphones",
